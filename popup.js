61617e5c9b43dfa76a969cdbb4d861329aad7449
Douglas Bainbridge <douglasb@here.local>
2021-01-18 14:36:34 +0000

    second commit

--- popup.js
+++ popup.js
@@ -1,12 +1,14 @@
 
 function display(results) {
     var data = results[0]
-    var keys = Object.keys(data)
+    var dates = data.dates
+    var keys = Object.keys(dates)
     var text = ''
     for (var i = 0; i < keys.length; ++i) {
-        text = text + '<div>' + keys[i] + ': ' + data[keys[i]] + '</div>'
+        text = text + '<div style=\'margin-bottom: 5px;\'><h3 style=\'margin-bottom: 0px;\'>' + keys[i] + '</h3><div>Active: ' + dates[keys[i]].active + '</div><div>Pending: ' + dates[keys[i]].pendingCompletion + '</div><div>Cancelled: ' + dates[keys[i]].cancelled + '</div><div>Other: ' + dates[keys[i]].other + '</div><div>Total: ' + dates[keys[i]].total + '</div></div>'
     }
-    document.querySelector("#id1").innerHTML = text
+    text = text + '<div></div>'
+    document.getElementById("textBox").innerHTML = text
 }
 
 function code() {
@@ -14,29 +16,62 @@ function code() {
     var tbody = tbl.firstElementChild
     var totalRows = tbody.children.length
 
+    var total = 0
     var totalActive = 0
+    var totalPendingCompletion = 0
+    var totalOther = 0
     var dates = {
 
     }
     for (var i = 1; i < totalRows; ++i) { //skip first row!
+
         var item = tbody.children[i];
-        if (item.innerHTML.includes('<span class="active">Active</span>')) {
-            totalActive++
-        }
         var firstCol = item.firstElementChild
         var date = firstCol.textContent.substring(0, 10)
+
+        // check objects initialised
         if (dates[date] == undefined) {
-            dates[date] = 1
+            dates[date] = {}
+        }
+        if (dates[date].active == undefined) {
+            dates[date].active = 0
+        }
+        if (dates[date].pendingCompletion == undefined) {
+            dates[date].pendingCompletion = 0
+        }
+        if (dates[date].cancelled == undefined) {
+            dates[date].cancelled = 0
+        }
+        if (dates[date].other == undefined) {
+            dates[date].other = 0
+        }
+        if (dates[date].total == undefined) {
+            dates[date].total = 0
+        }
+
+        // update counters
+        total++
+        dates[date].total++
+        if (item.innerHTML.includes('<span class="active">Active</span>')) {
+            dates[date].active++
+            totalActive++
+        } else if (item.innerHTML.includes('<span class="warning">Pending</span> <br><small>awaiting completion</small>')) {
+            dates[date].pendingCompletion++
+            totalPendingCompletion++
+        } else if (item.innerHTML.includes('<span class="cancelled">Cancelled</span>')) {
+            dates[date].cancelled++
+
         } else {
-            dates[date]++
+            dates[date].other++
+            totalOther++
         }
 
     }
-    console.log('total rows', totalRows)
-    console.log('total active', totalActive)
-    console.log(dates)
-
-    return dates
+    var data = {
+        total, totalActive, totalPendingCompletion, totalOther, dates
+    }
+    console.log(data)
+    return data
 }
 
 chrome.tabs.query({ active: true }, function (tabs) {
